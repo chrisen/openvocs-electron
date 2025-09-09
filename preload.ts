caf2fb10c213fdd4148b6ea50e17c66ffc04ecd9
chrisen <christopherendres@me.com>
2025-09-09 11:58:57 +0200

    Init minimal Electron kiosk app

---
+++ preload.ts
@@ -0,0 +1,7 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const style = document.createElement('style');
+  style.textContent = '* { cursor: none !important; }';
+  document.head.appendChild(style);
+});
+
+export {};
